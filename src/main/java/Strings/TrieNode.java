f44a765a65a2a45f13e7b6f18bb5cece036305d6
venuNarra <venu.parvs@gmail.com>
2018-01-28 11:25:59 +0530

    Create TrieNode.java
    new file

---
+++ src/main/java/Strings/TrieNode.java
@@ -0,0 +1,43 @@
+package Strings;
+
+public class TrieNode {
+	char data;
+	boolean isEndOfString;
+	TrieNode child[]=new TrieNode[26];
+	
+	TrieNode() {
+		// TODO Auto-generated constructor stub
+		isEndOfString=false;
+		for(int i=0;i<26;i++)
+			child[i]=null;
+	}
+	TrieNode(char c) {
+		this();
+		data=c;	
+	}
+
+	public char getData() {
+		return data;
+	}
+
+	public void setData(char data) {
+		this.data = data;
+	}
+
+	public boolean isEndOfString() {
+		return isEndOfString;
+	}
+
+	public void setEndOfString(boolean isEndOfString) {
+		this.isEndOfString = isEndOfString;
+	}
+
+	public TrieNode[] getChild() {
+		return child;
+	}
+
+	public void setChild(TrieNode[] child) {
+		this.child = child;
+	}
+	
+}
